27631861201d610886b0815ab3f6d39c5b3242eb
Falkreon <falkreon@gmail.com>
2017-06-08 15:50:28 -0500

    document helloworld

--- example/helloworld.asm
+++ example/helloworld.asm
@@ -1,10 +1,20 @@
+; sys.writeln parameters work as follows:
+; PG0 - page the string is in
+; R1  - offset of the string
+; R2  - max string length
+; R3  - descriptor to write the string to
+; This program just stuffs CS:str into PG0:R1, the length
+;   into R2, and a hardcoded descriptor number into R3 (1==stdout),
+;   and calls 0x05150006, which is the symbolic constant for
+;   sys.writeln
+
 		JMP start
 db str  "Hello, world!"
 start:
-		MOV PG0, CS
-		MOV R1, str
-		MOV R2, 13
+		MOV PG0, CS       ; string is in the same page as code
+		MOV R1, str       ; offset of string
+		MOV R2, 13        ; string is 13 bytes
 		MOV R3, 1         ; stdout descriptor
-		CALL 0x05150006   ; std.writeln
-		MOV R0, 0
-		RET
+		CALL 0x05150006   ; sys.writeln
+		MOV R0, 0         ; exit code 0
+		RET               ; return to OS control
